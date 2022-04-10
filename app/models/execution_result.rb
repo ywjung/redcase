7376f065c151ddc798cb4da1f654f89b8cc98313
Yongwook Jung <ywjung99@naver.com>
2022-04-10 17:36:19 +0900

    Update execution_result.rb

--- app/models/execution_result.rb
+++ app/models/execution_result.rb
@@ -2,10 +2,19 @@
 # TODO: Obsolete class, must be deleted after database migrate refactoring.
 # TODO: Really?
 
-class ExecutionResult < ActiveRecord::Base
+#class ExecutionResult < ActiveRecord::Base
 
-	unloadable
-	attr_protected :id
+#	unloadable
+#	attr_protected :id
 
+#end
+
+def create
+  ExecutionResult.create(executionresult_params)
 end
 
+private
+
+def executionresult_params
+  params.require(:id).permit(:name)
+end
