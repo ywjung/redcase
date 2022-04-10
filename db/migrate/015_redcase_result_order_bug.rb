70a13d703df1f62e8d279f34fe85e7c077f1b58a
Yongwook Jung <ywjung99@naver.com>
2022-04-10 17:29:43 +0900

    Update 015_redcase_result_order_bug.rb

--- db/migrate/015_redcase_result_order_bug.rb
+++ db/migrate/015_redcase_result_order_bug.rb
@@ -1,5 +1,5 @@
 
-class RedcaseResultOrderBug < ActiveRecord::Migration
+class RedcaseResultOrderBug < ActiveRecord::Migration[5.1]
 
   def self.up
     rename_column :execution_results, :order, :order_num
