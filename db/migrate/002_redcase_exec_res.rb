75be05396352d8f90199d5f581469e7224f51248
Yongwook Jung <ywjung99@naver.com>
2022-04-10 17:40:19 +0900

    Update 002_redcase_exec_res.rb

--- db/migrate/002_redcase_exec_res.rb
+++ db/migrate/002_redcase_exec_res.rb
@@ -2,15 +2,15 @@
 class RedcaseExecRes < ActiveRecord::Migration[5.1]
 
   def self.up
-    create_table :execution_results do |t|
-      t.string :name, :null => false
-    end
-    add_index :execution_results, [:name], :unique => true
-    ExecutionResult.create(:name => 'Passed')
-    ExecutionResult.create(:name => 'Failed')
-    ExecutionResult.create(:name => 'Blocked')
-    ExecutionResult.create(:name => 'Not Run')
-    ExecutionResult.create(:name => 'Not Available')
+#    create_table :execution_results do |t|
+#      t.string :name, :null => false
+#    end
+#    add_index :execution_results, [:name], :unique => true
+#    ExecutionResult.create(:name => 'Passed')
+#    ExecutionResult.create(:name => 'Failed')
+#    ExecutionResult.create(:name => 'Blocked')
+#    ExecutionResult.create(:name => 'Not Run')
+#    ExecutionResult.create(:name => 'Not Available')
   end
   
   def self.down
