66d8e1376b5e47ac695dc45d0faf4f3baf9d773a
Yongwook Jung <ywjung99@naver.com>
2022-04-10 17:27:39 +0900

    Update 004_redcase_patch_journal.rb

--- db/migrate/004_redcase_patch_journal.rb
+++ db/migrate/004_redcase_patch_journal.rb
@@ -1,5 +1,5 @@
 
-class RedcasePatchJournal < ActiveRecord::Migration
+class RedcasePatchJournal < ActiveRecord::Migration[5.1]
 
   def self.up
     add_column :execution_journals, :executor_id, :integer, :null => false
