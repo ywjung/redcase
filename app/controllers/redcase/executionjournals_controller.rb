38c5c3361d5241d404a10acc6991fb03bd759d6a
Yongwook Jung <ywjung99@naver.com>
2022-04-11 09:03:04 +0900

    Update executionjournals_controller.rb

--- app/controllers/redcase/executionjournals_controller.rb
+++ app/controllers/redcase/executionjournals_controller.rb
@@ -2,7 +2,7 @@
 class Redcase::ExecutionjournalsController < ApplicationController
 
 	unloadable
-	before_filter :find_project, :authorize
+	before_action :find_project, :authorize
 
 	def index
 		journals =
